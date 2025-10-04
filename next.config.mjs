1d3993d0c818042d5cb79bda2fd1cfa62f183eab
PakapornPhumyoo <littlecutecumber27@gmail.com>
2025-10-04 17:04:49 +0700

    Edited name for next.config.ts to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  // การตั้งค่าอื่นๆ ที่คุณมี
+}
+
+export default nextConfig
